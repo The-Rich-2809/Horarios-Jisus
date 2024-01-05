13abc602993af45c948ecfa92d1d640851714c42
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2024-01-05 04:56:09 -0600

    Hola

---
+++ Forms/Prefecto.cs
@@ -0,0 +1,171 @@
+﻿using Proyecto_Final.Clases;
+using Proyecto_Final.SQL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Final.Forms
+{
+    public partial class Prefecto : Form
+    {
+        public Prefecto()
+        {
+            InitializeComponent();
+        }
+        Reportes reportes = new Reportes();
+        Reportes_SQL reportes_SQL = new Reportes_SQL();
+        DataTable clases;
+        private void Prefecto_Load(object sender, EventArgs e)
+        {
+            Inicio();
+        }
+        public void Inicio()
+        {
+            Reportes_SQL reportes_SQL = new Reportes_SQL();
+            Dgv_Reportes.DataSource = reportes_SQL.Mostrar_Reportes();
+            foreach(Control c in this.Gb_Datos.Controls)
+            {
+                if(c is ComboBox || c is TextBox)
+                {
+                    c.Text = "";
+                }
+            }
+            Gb_Datos.Enabled = false;
+            Gb_Acciones.Enabled = true;
+            foreach(Control c in this.Gb_Acciones.Controls)
+            {
+                c.Enabled = false;
+            }
+            Btn_Nuevo.Enabled = true;
+            Btn_Eliminar.Enabled = true;
+        }
+        public bool Validar()
+        {
+            foreach(Control c in this.Gb_Datos.Controls)
+            {
+                if(c.Text == "")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void Btn_Nuevo_Click(object sender, EventArgs e)
+        {
+            Gb_Datos.Enabled = true;
+            foreach(Control c in this.Gb_Datos.Controls)
+            {
+                c.Enabled = false;
+            }
+            label1.Enabled = true;
+            Cb_Turno.Enabled = true;
+
+            foreach(Control c in this.Gb_Acciones.Controls)
+            {
+                c.Enabled = true;
+            }
+            Btn_Nuevo.Enabled = false;
+            Btn_Eliminar.Enabled = false;
+        }
+        private void Btn_Agregar_Click(object sender, EventArgs e)
+        {
+            if(Validar())
+            {
+                reportes.IdReporte = reportes_SQL.generaIdUsuario();
+                reportes.Turno = Cb_Turno.Text;
+                reportes.Grupo= Cb_Grupo.Text;
+                reportes.Materia = Cb_Materia.Text;
+                reportes.DiaReporte = Dtp_DiaReporte.Value.Year + "/"+ Dtp_DiaReporte.Value.Month + "/"+ Dtp_DiaReporte.Value.Day;
+                reportes.Reporte = Txt_Reporte.Text;
+                if(reportes_SQL.New_Reporte(reportes))
+                {
+                    MessageBox.Show(reportes_SQL.Mensaje);
+                    Inicio();
+                }
+                else
+                {
+                    MessageBox.Show(reportes_SQL.Mensaje);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Faltan Campos a llenar");
+            }
+        }
+        private void Btn_Eliminar_Click(object sender, EventArgs e)
+        {
+            int RenglonSeleccionado = Dgv_Reportes.CurrentRow.Index;
+            string IdClase = Dgv_Reportes.Rows[RenglonSeleccionado].Cells[0].Value.ToString();
+            DialogResult Resultado = MessageBox.Show("¿Desea eliminar el reporte con Id " + IdClase + "?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (Resultado == DialogResult.Yes)
+            {
+                if(reportes_SQL.Eliminar_Reporte(IdClase))
+                {
+                    MessageBox.Show(reportes_SQL.Mensaje);
+                    Inicio();
+                }
+                else
+                {
+                    MessageBox.Show(reportes_SQL.Mensaje);
+                }
+            }
+        }
+        private void Btn_Cancelar_Click(object sender, EventArgs e)
+        {
+            Inicio();
+        }
+
+        private void Cb_Turno_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Gb_Datos.Controls)
+            {
+                c.Enabled = false;
+            }
+            label1.Enabled = true;
+            Cb_Turno.Enabled = true;
+            label2.Enabled = true;
+            Cb_Grupo.Enabled = true;
+            Cb_Grupo.Text = "";
+            Cb_Materia.Text = "";
+            clases = reportes_SQL.Llenar_Grupo(Cb_Turno.Text);
+            Cb_Grupo.Items.Clear();
+            for(int i = 0; i < clases.Rows.Count; i++)
+            {
+                Cb_Grupo.Items.Add(clases.Rows[i]["Grupo"].ToString());
+            }
+        }
+        private void Cb_Grupo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Gb_Datos.Controls)
+            {
+                c.Enabled = false;
+            }
+            label1.Enabled = true;
+            Cb_Turno.Enabled = true;
+            label2.Enabled = true;
+            Cb_Grupo.Enabled = true;
+            label3.Enabled = true;
+            Cb_Materia.Enabled = true;
+            Cb_Materia.Text = "";
+            clases = reportes_SQL.Llenar_Materia(Cb_Grupo.Text);
+            Cb_Materia.Items.Clear();
+            for (int i = 0; i < clases.Rows.Count; i++)
+            {
+                Cb_Materia.Items.Add(clases.Rows[i]["Materia"].ToString());
+            }
+        }
+        private void Cb_Clase_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label4.Enabled = true;
+            Txt_Reporte.Enabled = true;
+            label5.Enabled = true;
+            Dtp_DiaReporte.Enabled = true;
+        }
+    }
+}
---
+++ SQL/Reportes_SQL.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Proyecto_Final.Clases;
+
+namespace Proyecto_Final.SQL
+{
+    internal class Reportes_SQL
+    {
+        public string Mensaje { get; set; }
+        public DataTable Mostrar_Reportes()
+        {
+            DataTable tablaReportes = new DataTable();
+
+            using (SqlConnection conexion = Conexion.Conectar())
+            {
+                SqlCommand cmdSelect;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                string sentencia = "select * from Reportes ";
+                try
+                {
+                    cmdSelect = new SqlCommand(sentencia, conexion);
+                    adapter.SelectCommand = cmdSelect;
+                    conexion.Open();
+                    adapter.Fill(tablaReportes);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                return tablaReportes;
+            }
+        }
+        public DataTable Llenar_Grupo(string turno)
+        {
+            DataTable tablaClases = new DataTable();
+
+            using (SqlConnection conexion = Conexion.Conectar())
+            {
+                SqlCommand cmdSelect;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                string sentencia = "select * from Clases where Turno = @Turno";
+                try
+                {
+                    cmdSelect = new SqlCommand(sentencia, conexion);
+
+                    adapter.SelectCommand = cmdSelect;
+                    cmdSelect.Parameters.AddWithValue("@Turno", turno);
+                    conexion.Open();
+                    adapter.Fill(tablaClases);
+                }
+                catch (Exception ex)
+                {
+                    Mensaje = ex.Message;
+                }
+
+                return tablaClases;
+            }
+        }
+        public DataTable Llenar_Materia(string grupo)
+        {
+            DataTable tablaClases = new DataTable();
+
+            using (SqlConnection conexion = Conexion.Conectar())
+            {
+                SqlCommand cmdSelect;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                string sentencia = "select * from Clases where Grupo = @Grupo";
+                try
+                {
+                    cmdSelect = new SqlCommand(sentencia, conexion);
+                    adapter.SelectCommand = cmdSelect;
+                    cmdSelect.Parameters.AddWithValue("@Grupo", grupo);
+                    conexion.Open();
+                    adapter.Fill(tablaClases);
+                }
+                catch (Exception ex)
+                {
+                    Mensaje = ex.Message;
+                }
+
+                return tablaClases;
+            }
+        }
+        public bool New_Reporte(Reportes reportes)
+        {
+            bool Operacion = false;
+
+            using (SqlConnection conexion = Conexion.Conectar())
+            {
+                SqlCommand Consulta;
+                int resultado = 0;
+                string SentenciaSQL = @"Insert Into Reportes Values (@Id,@Grupo,@Turno,@Materia,@DiaReporte,@Reporte)";
+                Consulta = new SqlCommand(SentenciaSQL, conexion);
+
+                Consulta.Parameters.AddWithValue("@Id", reportes.IdReporte);
+                Consulta.Parameters.AddWithValue("@Grupo", reportes.Grupo);
+                Consulta.Parameters.AddWithValue("@Turno", reportes.Turno);
+                Consulta.Parameters.AddWithValue("@Materia", reportes.Materia);
+                Consulta.Parameters.AddWithValue("@DiaReporte", reportes.DiaReporte);
+                Consulta.Parameters.AddWithValue("@Reporte", reportes.Reporte);
+                try
+                {
+                    conexion.Open();
+                    resultado = Consulta.ExecuteNonQuery();
+                    if (resultado > 0)
+                    {
+                        Mensaje = "Reporte registrado correctamente";
+                        Operacion = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Mensaje = ex.Message;
+                }
+            }
+            return Operacion;
+        }
+        public bool Eliminar_Reporte(string i)
+        {
+            using (SqlConnection conexion = Conexion.Conectar())
+            {
+                SqlCommand cmdCreate;
+                int filasafectadas;
+                string sentencia = @"delete from Reportes where IdReporte = @id";
+                cmdCreate = new SqlCommand(sentencia, conexion);
+                cmdCreate.Parameters.AddWithValue("@id", Convert.ToString(i));
+                try
+                {
+                    conexion.Open();
+                    filasafectadas = cmdCreate.ExecuteNonQuery();
+                    if (filasafectadas > 0)
+                    {
+                        Mensaje = "Reporte elimidado exitosamente";
+                        return true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Mensaje = ex.Message;
+                }
+            }
+            return false;
+        }
+
+        public int generaIdUsuario()
+        {
+            object tmp;
+            int IdReporte = 0;
+            using (SqlConnection conexion = Conexion.Conectar())
+            {
+                SqlCommand cmdCreate;
+                string sentencia = "select max(IdReporte) from Reportes";
+                try
+                {
+                    cmdCreate = new SqlCommand(sentencia, conexion);
+                    conexion.Open();
+                    tmp = cmdCreate.ExecuteScalar();
+                    if (tmp.Equals(DBNull.Value))
+                    {
+                        IdReporte = 1;
+                    }
+                    else
+                    {
+                        IdReporte = Convert.ToInt32(tmp);
+                        IdReporte++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            return IdReporte;
+        }
+
+    }
+}
